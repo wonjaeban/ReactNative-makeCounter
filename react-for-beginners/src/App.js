a81e469e2d7e8c0f55a0dab68aeb9be4c2355480
Wonjae <wonjae0709@naver.com>
2022-07-04 23:06:30 +0900

    1

---
+++ react-for-beginners/src/App.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+function Hello() {
+  
+  useEffect(() => {
+    console.log("Hi :)");
+    return () => console.log("bye :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      {showing ? <Hello/> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
+
+export default App;
